67f038d78cbfc0dbe5bea0d6972b3d0cc7e68efa
clarenochieng <98944975+clarenochieng@users.noreply.github.com>
2023-08-05 00:00:13 -0400

    Update Connection.py

--- Connection.py
+++ Connection.py
@@ -1,5 +1,6 @@
-!pip install streamlit
-!pip install qdrant-client
+//Install dependencies
+//!pip install streamlit
+//!pip install qdrant-client
 
 import streamlit as st
 from streamlit.connections import ExperimentalBaseConnection
@@ -11,3 +12,14 @@ from google.cloud import bigquery
 
 connection = st.experimental_connection()
 
+class MyConnection(ExperimentalBaseConnection[connection.MyConnection]):
+  def _connect(self, **kwargs) -> MyConnection:
+    return connection.connect(**self._secrets, **kwargs)
+  def cursor(self, **kwargs):
+    return self._instance(cursor)
+  ttl = 900
+  @st.cache_data(ttl = ttl)
+  def query(self, query: str, ttl = ttl, **kwargs):
+    return self._instance.query(query)
+
+st.title('Streamlit Challenge with Qdrant API')
